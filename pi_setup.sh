190561494a7a4d16b7a2dd62e2503d7202717481
Brian Hines <brian@projectweekend.net>
2014-06-08 17:27:16 -0500

    Add wifi setup

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo python scripts/set_wireless.py
 sudo python scripts/set_hostname.py
 sudo python scripts/set_boot_config.py
 sudo python scripts/set_ssh.py
---
+++ scripts/set_wireless.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from utils import file_templates
+
+
+def main():
+	user_input = raw_input("Want to setup wifi?: Y/N")
+	if user_input == 'Y':
+		ssid = ''
+		password = ''
+		while ssid == '' and password == '':
+			ssid = raw_input("Wifi network name (ssid): ")
+			password = raw_input("Wifi password: ")
+		update_file('/etc/network/interfaces', ssid, password)
+
+
+def update_file(path, ssid, password):
+	template_name = path.split('/')[-1]
+	new_file_data = file_templates.build(template_name, [ssid, password])
+	with open(path, 'w') as f:
+		f.write(new_file_data)
+
+
+if __name__ == '__main__':
+	main()
