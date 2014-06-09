ca40904195c66eaa36d3cd905af8e0528f251e7a
Brian Hines <brian@projectweekend.net>
2014-06-08 19:30:42 -0500

    Moving things around

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo python scripts/wireless.py
-sudo python scripts/hostname.py
-sudo python scripts/boot_config.py
-sudo python scripts/ssh.py
-sudo python scripts/system.py
+sudo python pi_setup/wireless.py
+sudo python pi_setup/hostname.py
+sudo python pi_setup/boot_config.py
+sudo python pi_setup/ssh.py
+sudo python pi_setup/system.py
