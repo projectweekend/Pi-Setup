005459b407fe6fef09198061b211edd85ee3b953
Brian Hines <brian@projectweekend.net>
2014-06-08 20:23:02 -0500

    These need to be strings

--- pi_setup/utils/validation.py
+++ pi_setup/utils/validation.py
@@ -5,5 +5,5 @@ def is_valid_hostname(new_hostname):
 
 
 def is_valid_gpu_mem(gpu_mem):
-    accepted_values = [16, 32, 64, 128, 256]
+    accepted_values = ["16", "32", "64", "128", "256"]
     return gpu_mem in accepted_values
