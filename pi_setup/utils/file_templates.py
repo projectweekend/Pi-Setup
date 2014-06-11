422fc57cd3ff37d0f77ebfc1dc4af3a2c45f5120
Brian Hines <brian@projectweekend.net>
2014-06-11 06:11:55 -0500

    Add template utility function

--- pi_setup/utils/file_templates.py
+++ pi_setup/utils/file_templates.py
@@ -6,3 +6,10 @@ def build(template_name, data):
     with open(file, 'r') as f:
         template = f.read()
     return template.format(**data)
+
+
+def load(template_name):
+    file = TEMPLATES_FOLDER + "/" + template_name
+    with open(file, 'r') as f:
+        template = f.read()
+    return template
