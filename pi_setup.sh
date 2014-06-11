befe92ab16cb7a42d3c029472dcf3159680de7bd
Brian Hines <brian@projectweekend.net>
2014-06-11 06:12:19 -0500

    Add i2c config to setup script

--- pi_setup.sh
+++ pi_setup.sh
@@ -3,3 +3,4 @@ sudo python pi_setup/wireless.py
 sudo python pi_setup/hostname.py
 sudo python pi_setup/boot_config.py
 sudo python pi_setup/system.py
+sudo python pi_setup/i2c.py
---
+++ pi_setup/i2c.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import subprocess
+from utils import file_templates
+
+
+def main():
+    user_input = raw_input("Want to enable the I2C pins? (Y/N): ")
+    if user_input == 'Y':
+        update_file('/etc/modules')
+        subprocess.call(["apt-get", "-y", "install", "python-smbus"])
+        subprocess.call(["apt-get", "-y", "install", "i2c-tools"])
+        if os.path.isfile('/etc/modprobe.d/raspi-blacklist.conf'):
+            update_file('/etc/modprobe.d/raspi-blacklist.conf')
+
+
+def update_file(path):
+    template_name = path.split('/')[-1]
+    new_file_data = file_templates.load(template_name)
+    with open(path, 'w') as f:
+        f.write(new_file_data)
+
+
+if __name__ == '__main__':
+    main()
