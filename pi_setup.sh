067617ae2be691579f98ee6c6067607a9084f91b
Brian Hines <brian@projectweekend.net>
2014-06-12 06:05:55 -0500

    Don't run this as sudo

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo python pi_setup/system.py
-sudo python pi_setup/password.py
+python pi_setup/password.py
 sudo python pi_setup/hostname.py
 sudo python pi_setup/wireless.py
 sudo python pi_setup/boot_config.py
