7a6ae3290cdd7970e31201b9990a67c7fcc6de1d
Brian Hines <brian@projectweekend.net>
2014-11-23 10:48:29 -0600

    Update system install script

--- pi_setup/system.py
+++ pi_setup/system.py
@@ -5,6 +5,8 @@ import subprocess
 def main():
     subprocess.call(["apt-get", "update"])
     subprocess.call(["apt-get", "-y", "upgrade"])
+    subprocess.call(["apt-get", "-y", "install", "software-properties-common"])
+    subprocess.call(["apt-get", "-y", "install", "python-software-properties"])
     subprocess.call(["apt-get", "-y", "install", "python-dev"])
     subprocess.call(["apt-get", "-y", "install", "python-pip"])
     subprocess.call(["apt-get", "-y", "install", "avahi-daemon"])
