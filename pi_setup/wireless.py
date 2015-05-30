fb54680d5c4ce6e49c274c9c7a6078093e9620ea
Brian Hines <brian@projectweekend.net>
2015-05-30 07:49:44 -0500

    Clean up file

--- pi_setup/wireless.py
+++ pi_setup/wireless.py
@@ -4,31 +4,30 @@ from utils.installation import OptionalInstall
 
 
 def main():
-	prompt_txt = "Want to setup wifi? (Y/N): "
-	skip_txt = "Skipping wifi..."
+    prompt_txt = "Want to setup wifi? (Y/N): "
+    skip_txt = "Skipping wifi..."
 
-	def action():
-		ssid = ''
-		password = ''
-		while ssid == '' and password == '':
-			ssid = raw_input("Wifi network name (ssid): ")
-			password = raw_input("Wifi password: ")
-		update_file('/etc/network/interfaces', ssid, password)
+    def action():
+        ssid = ''
+        password = ''
+        while ssid == '' and password == '':
+            ssid = raw_input("Wifi network name (ssid): ")
+            password = raw_input("Wifi password: ")
+        update_file('/etc/network/interfaces', ssid, password)
 
-	OptionalInstall(prompt_txt, skip_txt, action).run()
-
+    OptionalInstall(prompt_txt, skip_txt, action).run()
 
 
 def update_file(path, ssid, password):
-	data = {
-		'ssid': ssid,
-		'password': password
-	}
-	template_name = path.split('/')[-1]
-	new_file_data = file_templates.build(template_name, data)
-	with open(path, 'w') as f:
-		f.write(new_file_data)
+    data = {
+        'ssid': ssid,
+        'password': password
+    }
+    template_name = path.split('/')[-1]
+    new_file_data = file_templates.build(template_name, data)
+    with open(path, 'w') as f:
+        f.write(new_file_data)
 
 
 if __name__ == '__main__':
-	main()
+    main()
